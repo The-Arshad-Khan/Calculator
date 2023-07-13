453804654bfc5bb7c27143c5fc41352aacb585e3
sonu kumar <sonukumar7505891@gmail.com>
2023-07-13 13:52:30 +0530

    new file added

---
+++ app/src/main/java/com/rianomusicskb/skbchat/lr.java
@@ -0,0 +1,14 @@
+package com.rianomusicskb.skbchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lr);
+    }
+}
